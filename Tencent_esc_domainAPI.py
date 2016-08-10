14dce7369ea1fd03da467c7369b7af3127ccc7e4
libanglong <libanglong@hotmail.com>
2016-08-10 17:57:44 +0800

    新增获取Singtxt方法

--- Tencent_esc_domainAPI.py
+++ Tencent_esc_domainAPI.py
@@ -5,4 +5,41 @@
  @DateTime:    2016-08-10 17:14:49
  @Description:
 '''
-fk
+import os
+from datetime import datetime
+import hashlib
+import random
+
+SecretId = 'xxxid'
+SecretKey = 'xxxkey'
+
+
+def get_all_machines(object):
+    my_domain = 'cvm.api.qcloud.com'  # 服务器请求域名
+    # 传说中的公共参数
+    data = {
+        "Action": 'DescribeInstance',
+        "Region": 'gz',
+        "Timestamp": datetime.utcnow(),
+        "Nonce": random.randint(0, 1024),
+        "SecretId": SecretId,
+    }
+    signTxt = generate_signtrue('GET', my_domain, **data)
+
+
+def generate_signtrue(method, url, **kwargs):
+    # 将字典按照键排序 计算hash值返回
+    fullUrl = '{0}/v2/index.php?'.format(url)
+    if isinstance(kwargs, dict):
+        try:
+            end = sorted(kwargs, key=lambda ykey: ykey[0])
+            endStr = "{0}{1}{2}".format(method, fullUrl, ''.join(end))
+            mysha = hashlib.sha1()
+            mysha.update(endStr.encode())
+            has_ret = mysha.hexdigest()
+            return has_ret
+        except Exception as e:
+            print(e)
+            return None
+    else:
+        return None
